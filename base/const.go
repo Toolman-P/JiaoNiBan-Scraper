aeadcce91e8bb9ff8ebaf7d0dec2d7754bbaa563
ToolmanP <ToolmanP@outlook.com>
2021-11-24 17:27:08 +0800

    added const.go for consts

---
+++ base/const.go
@@ -0,0 +1,6 @@
+package base
+
+var UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"
+var DeanBaseURL = "http://www.jwc.sjtu.edu.cn"
+var DeanFirstPage = "http://www.jwc.sjtu.edu.cn/index/mxxsdtz.htm"
+var DeanPrefix = "http://www.jwc.sjtu.edu.cn/index/"
