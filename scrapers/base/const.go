673d454417496f8ba29709edb2d47710ea2d1c8d
ToolmanP <ToolmanP@outlook.com>
2021-12-22 21:00:49 +0800

    #4 added Rabbitmq

--- scrapers/base/const.go
+++ scrapers/base/const.go
@@ -1,7 +1,6 @@
 package base
 
 const (
-	UserAgent   = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"
-	storagePath = "downloads/website"
-	staticPath  = "static/website"
+	UserAgent  = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"
+	staticPath = "static/website"
 )
--- scrapers/base/vars.go
+++ scrapers/base/vars.go
@@ -1,3 +1,6 @@
 package base
 
+import "os"
+
 var baseurlMap = baseMap{"dean": "http://www.jwc.sjtu.edu.cn"}
+var storagePath = os.Getenv("STORAGEPATH")
